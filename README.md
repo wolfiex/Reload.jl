2a446fbe26b372a019125d0a9ab43bcde6a3004b
Daniel Ellis <daniel.ellis.research@gmail.com>
2017-10-13 22:59:14 +0100

    error handling on nonfiles

--- README.md
+++ README.md
@@ -5,3 +5,22 @@
 [![Coverage Status](https://coveralls.io/repos/wolfiex/Reload.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/wolfiex/Reload.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/wolfiex/Reload.jl/coverage.svg?branch=master)](http://codecov.io/github/wolfiex/Reload.jl?branch=master)
+
+A simple package that reruns the selected script upon modification. 
+
+## Install
+
+``` julia 
+    Pkg.add("Reload")
+```
+
+## Usage
+
+``` julia
+
+        using Reload
+             
+        start("./path/to/filename.jl")
+        
+```
+        
--- src/Reload.jl
+++ src/Reload.jl
@@ -2,4 +2,44 @@ module Reload
 
 # package code goes here
 
+println("\x1b[34m\ ")
+print("Reload.jl loaded")
+println("\x1b[00m\ ")
+
+function watch(filename)
+
+    start = stat(filename).mtime
+
+    while start === stat(filename).mtime
+
+        sleep(1);
+
+    end
+    
+    eval(parse("try include(\"$(filename)\");catch e; println(\"\x1b[31m\ ERROR\");print(e);println(\"\x1b[00m\ \"); end;start(\"$(filename)\")"))
+
+
+
+end 
+
+
+
+function start(filename)
+
+    if (isfile(filename) == false) 
+        error("No such file: "*filename)
+        return 
+    end    
+
+    @async (sleep(5); watch(filename))
+    
+    println("\x1b[34m\ ")
+    println("watching $(filename)")
+    println("\x1b[00m\ ")
+
+end
+
+
+
+
 end # module
